7dafbea0c7021b8f4c65a77dac7c29c7a750a825
sandygrandy <langeng.sandra@gmail.com>
2024-12-04 21:12:16 +0100

    home page done +++
    initial home page done as well as some other tweaks. hanging a little behind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    './**/*.{html,js}', '!./node_modules/**/*'
-  ],
+  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
     extend: {
       colors: {
-        primary: '#F07863',
-        secondary: '#FFF8EE',
-        complimentary: '#5AC2A7',
-        dark: '#2C2C2C',
-        card: '#DAD8CD',
-      }
+        primary: "#F07863",
+        secondary: "#FFF8EE",
+        complimentary: "#5AC2A7",
+        dark: "#2C2C2C",
+        card: "#DAD8CD",
+      },
+    },
+    fontFamily: {
+      headers: ["Fredoka one"],
+      paragraphs: ["Inter"],
     },
   },
   plugins: [],
-}
-
+};
